f884b49f8dafe1a3a90e739b52e2901ed8909acd
Tamás Szelei <szelei.t@gmail.com>
2020-02-09 17:17:26 +0000

    cblack, baby

--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@ from distutils.core import setup
 from Cython.Build import cythonize
 import numpy
 
-setup(ext_modules = cythonize("samplerbox_audio.pyx", build_dir="build"),
-      compiler_directives={'language_level' : "3"},
-      include_dirs=[numpy.get_include()])
+setup(
+  ext_modules=cythonize("samplerbox_audio.pyx", build_dir="build"),
+  compiler_directives={"language_level": "3"},
+  include_dirs=[numpy.get_include()],
+)
